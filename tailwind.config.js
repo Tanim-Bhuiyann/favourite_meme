e633ccffcfecffe8c919a88dba42f7204640f0f4
Tanim-Bhuiyann <tanimh64@gmail.com>
2024-07-14 01:09:30 +0600

    working

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+import daisyui from "daisyui";
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ["./*.html" ],
+  theme: {
+    container:{
+      center: true,
+      padding:"20px",
+     
+    },
+    extend: {},
+  },
+  plugins: [daisyui],
+}
+
